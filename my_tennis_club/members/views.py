dd7da24d6bc14d215d9ecfdeac089c2c46837f2a
Arjay Arceo <arjay.m.arceo@gmail.com>
2023-09-10 12:10:16 +0800

    Django Display Data

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,31 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
 
 def members(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+# FOR TESTING
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))
--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--*=3ev0!^gqi5bn75sg-(ernuz0!qremmcg#^z$%*ya11nshan"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
